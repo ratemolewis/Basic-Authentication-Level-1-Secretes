adf310d8fcc1b437354a34592fba0eab5dbdc0dc
Ratemo Lewis <retemolewis@gmail.com>
2023-05-17 13:11:54 +0300

    LEVEL 6 AUTHENTICATION: Google OAuth 2.0

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
 
 const app = express();
 
@@ -26,23 +28,65 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB",{useNewUrlParser: true});
 //
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId:String
 }); 
 //passportLocalMongoose, used to hash and salt users passwords as well as to save them 
 userSchema.plugin(passportLocalMongoose);
- 
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User",userSchema);
 // CHANGE: USE "createStrategy" INSTEAD OF "authenticate" 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//serialize and deserialize universal methods for working with different stategies
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.id,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+//setup our googlestategy and configure. 
+passport.use(new GoogleStrategy({
+    clientID:  process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  //callback function
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/auth/google",
+//use passport to authenticate our users, using the google stategy. once we hit google we tell them we need the users profile.
+  passport.authenticate("google", { scope: ["profile"] }
+  ));
+
+// This get request is made by google with the users details/callback,
+  app.get("/auth/google/secrets", 
+  //Authenticae the use inside our app.
+  passport.authenticate("google", { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect secrets.
+    res.redirect('/secrets');
+  });
 
 app.get("/register", (req, res) => {
     res.render("register");
@@ -51,6 +95,7 @@ app.get("/register", (req, res) => {
 app.get("/login", (req, res) => {
     res.render("login");
 });
+
 
 app.get("/secrets", function (req, res){
  if(req.isAuthenticated()){
